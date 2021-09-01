f74e802f451a279a5688685c5471a01fa8a25fe0
Stephen Yeboah <stephenoyeboah@gmail.com>
2021-09-01 09:25:22 -0700

    add ScreenTest

--- test/com/graduation/client/TitleScreenTest.java
+++ test/com/graduation/client/TitleScreenTest.java
@@ -1,10 +1,12 @@
 package com.graduation.client;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TitleScreenTest {
 
-    @org.junit.Test
-    public String inputOptions() {
+    @Test
+    public void inputOptions() {
     }
 }
