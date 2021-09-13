27591243b47bf23f15ce85c357930425a87aa9bf
josephlee3454 <joseph.lee3454@gmail.com>
2021-09-13 09:24:13 -0700

    added file reader tests

--- test/com/graduation/reader/ReaderTest.java
+++ test/com/graduation/reader/ReaderTest.java
@@ -1,10 +1,14 @@
 package com.graduation.reader;
 
+import com.graduation.elements.Player;
+import com.graduation.utils.Grade;
 import com.graduation.utils.TextFileReader;
+import com.graduation.utils.readMap;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static com.graduation.utils.Grade.*;
 import static org.junit.Assert.*;
 
 public class ReaderTest {
@@ -37,6 +41,28 @@ public class ReaderTest {
         TextFileReader tfr2 = new TextFileReader();
         assertEquals(tfr.readFightFile(),tfr2.readFightFile());
     }
+
+    @Test
+    public void testReadMapwithNormalObject() throws Exception{
+        Player player = new Player("joe", 2, 5,JUNIOR,"Maryland");
+        assertFalse(readMap.convertedMap().isEmpty());
+    }
+    @Test
+    public void testReadMapwithNegativeIntsObject() throws Exception{
+        Player player = new Player("joe", -2, -5,JUNIOR,"Maryland");
+        assertFalse(readMap.convertedMap().isEmpty());
+    }
+    @Test
+    public void testReadMapwithNegativeManyObjects() throws Exception{
+        Player player1 = new Player("joe", -2, -5,JUNIOR,"Maryland");
+        Player player2 = new Player("mary", -4, 7,SOPHOMORE,"Maryland");
+        Player player3 = new Player("mary-joe", -2, 95,SENIOR,"Maryland");
+        Player player4 = new Player("nancy", -2, -5,JUNIOR,"Maryland");
+        Player player5 = new Player("john-smith", -2, -5,JUNIOR,"Maryland");
+
+        assertFalse(readMap.convertedMap().isEmpty());
+    }
+
 
 
 
